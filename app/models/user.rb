dbd7fa199e77dec9d67fd2b50e11416ce3dc5a93
orkoselvis <ivanviter@i.ua>
2019-01-17 22:01:55 +0200

    Added all code to account activation branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,27 +30,38 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-    def authenticated?(remember_token)
-      return false if remember_digest.nil?
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
   # Forgets a user.
-    def forget
-      update_attribute(:remember_digest, nil)
-    end
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
-    private
+  # Activates an account.
+  def activate
+    update_attribute(:activated, true, :activated_at, Time.zone.now)
+  end
 
-    # Converts email to all lower-case.
-    def downcase_email
-      self.email = email.downcase
-    end
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  private
+
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 
 end
