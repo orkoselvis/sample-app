2fe7222a09f455c3d6dddcc383f623190e868c23
Orko Elvis <ivanviter@i.ua>
2019-02-06 23:08:10 +0200

    Added some shit

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -6,23 +6,26 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     @user = users(:viter)
   end
 
+
   test "micropost interface" do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=FILL_IN]'
     # Invalid submission
-    assert_no_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: "" } }
-    end
+    post microposts_path, params: { micropost: { content: "" } }
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
-    end
-    assert_redirected_to root_url
-    follow_redirect!
-    assert_match content, response.body
+      post microposts_path, params: { micropost:
+                                  { content: content,
+                                    picture: FILL_IN } }
+  end
+  assert FILL_IN.picture?
+  follow_redirect!
+  assert_match content, response.body
     # Delete post
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
